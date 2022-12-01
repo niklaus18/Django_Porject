7f58ed5e0ac56a83ca5006efc145af77f65fdbae
niklaus18 <amangora1999@gmail.com>
2022-12-01 11:38:30 +0530

    Second step

---
+++ Readme.md
@@ -0,0 +1,38 @@
+
+## UML Association Diagram
+
+
+## Book
+| S. No | Param |
+| ----- | ----- |
+| 1 |  title |
+| 2 | author |
+| 3 | summary |
+| 4 | ISBN |
+| 5 | genere |
+| 6 | language |
+
+## Genere 
+
+| S. No. | Param |
+| --- | --- |
+| 1 | name |
+
+
+## Author 
+ 
+ | S. No. | Author |
+ | --- | --- |
+ | 1 | Name |
+ | 2 | Date of Birth |
+ | 3 | Date of Death |
+ | 4 | Book |
+
+ ## Book Instance 
+
+ | S. No. | BookInstance |
+ |  ----- | -----------|
+ | 
+
+
+
---
+++ catalog/models.py
@@ -0,0 +1,44 @@
+
+# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+class Book(models.Model):
+    """A typical class defining a model, derived from the Model class."""
+
+    # Fields
+    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
+    # …
+
+    # Metadata
+    class Meta:
+        ordering = ['-my_field_name']
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.my_field_name
+
+from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ['created']
